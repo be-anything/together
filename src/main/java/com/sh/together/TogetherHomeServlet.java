25e846353657b189c28e251c6bb8e0614273319b
minjeong <snailmin27@gmail.com>
2023-12-07 17:06:22 +0900

    Init: together app

---
+++ src/main/java/com/sh/together/TogetherHomeServlet.java
@@ -0,0 +1,18 @@
+package com.sh.together;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 변수명 일괄변경 shift + f6
+@WebServlet("/home")
+public class TogetherHomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        resp.getWriter().append("Hello world");
+    }
+}
